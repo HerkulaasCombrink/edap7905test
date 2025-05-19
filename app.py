f66ed21de990ace4bf9dfada61574a25b4a21737
HMVE Combrink <44698092+HerkulaasCombrink@users.noreply.github.com>
2025-05-19 15:38:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ import pandas as pd
 import streamlit as st
 
 # Public CSV file from GitHub
-url = "https://github.com/HerkulaasCombrink/edap7905test/blob/main/data.csv"
+url = "https://raw.githubusercontent.com/HerkulaasCombrink/edap7905test/main/data.csv"
 df = pd.read_csv(url)
 st.dataframe(df)
